3cf52f08727cb6d31a0cb75169deb958254f89ec
Carlyle Komalram <glaw14@gmail.com>
2024-10-05 14:05:49 -0500

    adding files to work in api call

---
+++ api.py
@@ -0,0 +1,9 @@
+from generate_branches import generate_branch_name
+
+def run():
+    generate_branch_name(456, "prueba desde archivo api")
+
+
+if __name__ == "__main__":
+    run()
+
--- generate_branches.py
+++ generate_branches.py
@@ -6,9 +6,9 @@ LANGUAGE_EN = 'en'
 MAX_LEN = 70
 
 def generate_branch_name(ticket_number, desc):
-
+    desc = translate_to_english(desc)
     normalized_desc = generate_normalize_desc(desc)
-    return {
+    options =  {
         "Feat": f"feat/{ticket_number}-{normalized_desc}",
         "Bug": f"fix/{ticket_number}-{normalized_desc}",
         "Pre Feat": f"feat/{ticket_number}-{normalized_desc}_pre",
@@ -20,6 +20,9 @@ def generate_branch_name(ticket_number, desc):
         "Commit for prod hotfix Feat": f"feat:[PATCH][{ticket_number}]-{normalized_desc}",
         "Commit for prod hotfix Bug": f"fix:[PATCH][{ticket_number}]-{normalized_desc}",
     }
+
+    for key, value in options.items():
+        print(f"{key}:\n {value}")
 
 def generate_normalize_desc(desc):
     normalized_desc = desc.lower().replace(' ', '-').replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u')
@@ -39,9 +42,6 @@ def run():
     create_parse.add_argument('-d', '--description', required=True, help="description of ticket")
     args = parser.parse_args()
 
-    result = generate_branch_name(args.ticket,translate_to_english(args.description))
-    for key, value in result.items():
-        print(f"{key}:\n {value}")
-
+    generate_branch_name(args.ticket,args.description)
 if __name__ == "__main__":
     run()
